074033df671b8579107ae17cf915f66da506c588
stuart teh <stuartteh1995@gmail.com>
2023-03-28 21:37:24 +1100

    adding googles news api

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,16 @@ fetch(proxyUrl + targetUrl, options)
   .then((response) => response.json())
   .then((response) => console.log(response))
   .catch((err) => console.error(err));
+
+  // api news
+  const apiKey = '96f2d661b6ac44e8a01323e41bd281d9'; // Replace with your News API key
+const searchTerm = 'melbourne'; // Declare the searchTerm variable and assign a value
+const url = `https://newsapi.org/v2/everything?q=${searchTerm}&apiKey=${apiKey}`; // Include searchTerm in the URL
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    console.log("News data:", data.articles);
+  })
+  .catch(error => {
+    console.error("Error fetching news data:", error);
+  });
